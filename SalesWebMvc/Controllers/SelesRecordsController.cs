c7d2fef86a556e9df96d3a2555cc93eb08375e79
Matheus Valle <mdovallesilva@gmail.com>
2022-11-01 14:10:25 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvc/Controllers/SelesRecordsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SelesRecordsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+    }
+}
